c65eea505c3fefbbd28c79f5928e2285880f4616
tahakapasi <t.s.m.kapasi@gmail.com>
2017-01-10 10:21:27 +0530

    Logged the accelerometer readings

--- app/src/main/java/com/pothole/potholedetector/Ride.java
+++ app/src/main/java/com/pothole/potholedetector/Ride.java
@@ -18,7 +18,10 @@ public class Ride extends Service implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
         if(mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            Log.d("Hello accelerometer", "onSensorChanged: Hi I detected a change !!");
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            Log.d("X,Y,Z", String.valueOf(x)+","+String.valueOf(y)+","+String.valueOf(z));
         }
     }
 
@@ -32,9 +35,9 @@ public class Ride extends Service implements SensorEventListener {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Let it continue running until it is stopped.
-        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
-        SensorManager senSensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        Sensor senAccelerometer=senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        SensorManager senSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        Sensor senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         return START_STICKY;
@@ -51,7 +54,6 @@ public class Ride extends Service implements SensorEventListener {
         SensorManager senSensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
         super.onDestroy();
         senSensorManager.unregisterListener(this);
-        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
     }
 
 }
